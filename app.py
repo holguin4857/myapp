c87a3483f3915a11fe5e09004d01396cdf279f9e
omar holguin <holguin30@hotmail.com>
2022-09-28 16:00:20 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    todo_text = db.Column(db.String(100), index = True)
+
+class TodoForm(FlaskForm):
+    todo = StringField("Todo")
+    submit = SubmitField("Add Todo")
+
+db.create_all()
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if 'todo' in request.form:
+        db.session.add(Todo(todo_text=request.form['todo']))#
+        db.session.commit()
+    return render_template('index.html', todos=Todo.query.all(), template_form=TodoForm())
